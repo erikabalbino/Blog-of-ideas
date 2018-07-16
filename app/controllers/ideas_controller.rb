ed70928494f53e1b3bc068e13045645f044c81b0
Erika Balbino <erk.balbino@gmail.com>
2018-07-16 12:41:35 -0700

    CRUD for Idea model

---
+++ app/controllers/ideas_controller.rb
@@ -0,0 +1,58 @@
+class IdeasController < ApplicationController
+
+    before_action :find_idea, only: [:show, :edit, :update, :destroy]
+
+
+    def new
+        @idea = Idea.new
+    end
+
+    def create
+        # render json: params
+
+        @idea = Idea.new ideas_params
+
+        if @idea.save
+            # render json: @idea
+
+            redirect_to idea_path(@idea.id)
+        else
+            render :new
+        end
+    end
+
+    def show
+    end
+
+    def edit
+    end
+    
+    def update 
+        if @idea.update(ideas_params)
+            redirect_to idea_path(@idea.id)
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @idea.destroy
+        redirect_to ideas_path
+    end
+
+    def index 
+        @ideas = Idea.all.order(created_at: :desc)
+        # render json: @ideas
+    end
+    
+
+    private
+
+    def ideas_params
+        params.require(:idea).permit(:title, :description)
+    end
+
+    def find_idea
+        @idea = Idea.find params[:id]
+    end
+end
